e00514e623411d5c516edf9a8881de8d7d371050
Kusastic <katin-box@yandex.ru>
2018-10-30 22:50:06 +0300

    added reading from file 6-arrays-2d

--- 6-arrays-2d/main.cpp
+++ 6-arrays-2d/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 
 using namespace std;
 
@@ -19,7 +20,7 @@ void proizv(int **a, int n)
 	{
 		int przvd = a[i][0], part = 1;
 
-		for (int j = 0; j < n-1; j++)
+		for (int j = 0; j < n - 1; j++)
 		{
 			if ((a[i][j] > 0) && (a[i][j + 1] > 0))
 			{
@@ -38,68 +39,70 @@ void proizv(int **a, int n)
 			cout << setw(22) << "Proizvedenie stroki: " << i + 1;
 			cout << " = " << przvd << endl;
 			//i+1 because array starts with 0
-		}		
+		}
 	}
 }
 
 int maxsumdiag(int **a, int n)
 {
-int sum = -1000;
+	int sum = -1000;
 
-for (int i = 1; i < n; i++)
-{
-	int sum1 = 0, sum2 = 0;
-
-	for (int j = 0, k = j; j < n - i; j++, k++)
+	for (int i = 1; i < n; i++)
 	{
-		sum1 += a[j + i][k];
-		sum2 += a[j][k + i];
-	}
+		int sum1 = 0, sum2 = 0;
 
-	if (sum1 >= sum)
-		sum = sum1;
-	if (sum2 >= sum)
-		sum = sum2;
-}
+		for (int j = 0, k = j; j < n - i; j++, k++)
+		{
+			sum1 += a[j + i][k];
+			sum2 += a[j][k + i];
+		}
+
+		if (sum1 >= sum)
+			sum = sum1;
+		if (sum2 >= sum)
+			sum = sum2;
+	}
 	return sum;
 }
 
- int main()
- {
+int main()
+{
 	int n;
-	cout << "Vvedite kolichestvo elements of array ^2 : ";
-		 cin >> n;
-	if (n <= 0)
-		 {
-			 cout << "Wrong data!!!";
-			 return 0;
-	     }
+	cout << "(n = kolichestvo strok ^2 in matrix)\n";
+	cout << "Enter 1 <= n <=10: ";
+	
+	cin >> n;
 
-	 int **a = new int* [n];
-	 
-	 for (int i = 0; i < n; i++)
-	 {
-		 a[i] = new int[n];
-	 }
+	int **a = new int*[n];
 
-	cout << "Vvedite znacheniya elements of array: ";
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < n; j++)
-			cin >> a[i][j];
+		a[i] = new int[n];
 	}
-	cout << endl;
+
+	ifstream fin("file.txt");
+	if (!fin.is_open()) 
+	{
+		cout << "Error!! Something went wrong!" << endl;
+		return 0;
+	}	
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)		
+			fin >> a[i][j];
+
+	fin.close();
 
 	vivodmas(a, n);
 
 	proizv(a, n);
 	int sum = maxsumdiag(a, n);
 	if (!(sum == -1000))
-	cout << setw(32) << "Max sum of diagonals in array: " << sum;
+		cout << setw(32) << "Max sum of diagonals in array: " << sum;
 
 	for (int i = 0; i < n; i++) //cleaning services
 	{
-		delete [] a[i] ;
+		delete[] a[i];
 	}
 	delete[] a;
 
---
+++ 7-strings/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	ifstream fin("file.txt");
+	fin.close();
+
+	return 0;
+}
