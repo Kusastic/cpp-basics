50ce1445e47ad227a50f046671792a8c472e139a
Kusastic <katin-box@yandex.ru>
2018-11-06 18:35:50 +0300

    added 7&8

---
+++ 08-structures/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int n = 7;
+
+struct AEROFLOT
+{
+	string nameflight, planetype;
+	int numberflight;
+};
+
+int main()
+{
+	AEROFLOT planes[n];
+
+	cout << "Vvedite 7 tipov samoletov:";
+	for (int i = 0; i < n; i++)
+		cin >> planes[i].planetype;
+
+	cout << "Vvedite 7 nomerov samoletov:";
+	for (int i = 0; i < n; i++)
+		cin >> planes[i].numberflight;
+
+	cout << "Vvedite 7 naznachenij samoletov:";
+	for (int i = 0; i < n; i++)
+		cin >> planes[i].nameflight;	
+
+	for (int i = 0; i < n - 1; i++)
+	{		
+		for (int j = 0; j < n - i - 1; j++)
+		{
+			if (planes[j].numberflight > planes[j + 1].numberflight)
+			{
+				int a = planes[j].numberflight;
+				planes[j].numberflight = planes[j + 1].numberflight;
+				planes[j + 1].numberflight = a;				
+			}
+		}		
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Plane " << planes[i].planetype;
+		cout << " number " << planes[i].numberflight;
+		cout << " is flying to " << planes[i].nameflight;
+		cout << endl;
+	}
+
+	string naznachenie;
+	cout << "Vvvedite naznachenie: ";
+	cin >> naznachenie;
+	cout << endl;
+
+	bool flight = false;
+	for (int i = 0; i < n; i++)
+	{
+		if (planes[i].nameflight == naznachenie)
+		{
+			flight = true;
+			cout << "Vam podhodit ";
+			cout << planes[i].planetype << " plane " ;
+			cout << "number " << planes[i].numberflight;			
+			cout << endl;
+		}
+	}
+
+	if (flight == false)
+		cout << "Vam ne podhodit ni odit samolet";	
+
+	return 0;
+}
--- 7-strings/main.cpp
+++ 7-strings/main.cpp
@@ -1,13 +1,38 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-	ifstream fin("file.txt");
+	string str;
+	char glansije [] = { 'E', 'Y', 'U', 'I', 'O', 'e', 'y', 'u', 'i', 'o'};
+
+	ifstream fin("text.txt");
+	if (!fin.is_open())
+	{
+		cout << "Error!! Something went wrong!\n" << endl;
+		return 0;
+	}
+
+	cout << "Slova nachinaushiesya s glasnih bukv: \n";
+
+	while (getline(fin, str)) 
+	{
+		for (unsigned int i = 0; i < str.length(); i++)
+			if (find(glansije, glansije + 10, str[i]) != (glansije + 10))
+				if ((i == 0) || ((str[i - 1] == ' ') && (i > 0)))
+				{
+					while (isalpha(str[i]))
+						cout << str[i++];
+					cout << endl;					
+				}
+	}
+
 	fin.close();
 
 	return 0;
-}
+}		
